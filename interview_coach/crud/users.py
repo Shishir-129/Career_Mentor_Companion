a47f42008c0819c2634877a38106b78c1f54e5cd
Sunil Paudel <sunilpaudel013@gmail.com>
2026-06-29 22:58:31 +0545

    added the different tables in models.py

---
+++ interview_coach/crud/users.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from pwdlib import PasswordHash
+
+from database.models import User
+from schemas.user import UserCreate
+
+password_hash = PasswordHash.recommended()
+
+def create_user(db: Session, user: UserCreate):
+    db_user = User(
+        fullname=user.fullname,
+        email=user.email,
+        password=password_hash.hash(user.password),
+        target_role = user.target_role
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_users(db: Session):
+    return db.query(User).all()
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def delete_user(db: Session, user_id: int):
+    user = get_user(db, user_id)
+    if not user:
+        return None
+    db.delete(user)
+    db.commit()
+    return user
---
+++ interview_coach/schemas/user.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    fullname: str
+    email: EmailStr
+    password: str
+    target_role:str
+
+class UserResponse(BaseModel):
+    id: int
+    fullname: str
+    email: str
+    target_role:str
+
+    class Config:
+        from_attributes = True
